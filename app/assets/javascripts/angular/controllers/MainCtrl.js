8e77976acdb75c17f6226cfb24b0eb715a86cee1
mroll <mpr56@drexel.edu>
2014-05-02 14:44:29 -0400

    added new questions

--- app/assets/javascripts/angular/controllers/MainCtrl.js
+++ app/assets/javascripts/angular/controllers/MainCtrl.js
@@ -10,6 +10,16 @@ App.controller('MainCtrl', [
 				'choices': { 'Yes': -10000, 'No': 0 } },
 			{ 'body': 'What is your timeframe?',
 				'choices': { '3 months': 10000, '4 months': 20000, '5 months': 30000 } },
+			{ 'body': 'Which devices will support your app?',
+				'choices': { 'Phone': 0, 'Tablet': 0 } },
+			{ 'body': 'Will your app work offline?',
+				'choices': { 'Yes': 0, 'No': 0 } },
+			{ 'body': 'Will your app support credit card payment?',
+				'choices': { 'Yes': 0, 'No': 0 } },
+			{ 'body': 'How will users sign on?',
+				'choices': { 'Username/Password': 0, 'Social Network Profile': 0, 'Active Directory': 0 } },
+			{ 'body': 'How many third party APIs will your app use?',
+				'choices': { 'None': 0, '1-3': 0, '3+': 0 } }
 		];
 
 		$scope.qCounter = 0;
