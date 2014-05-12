c30d421cf22233338fab4c440c87d25b3d8d849b
mroll <mpr56@drexel.edu>
2014-05-12 12:12:05 -0400

    attempting to get rails to save answers with user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 	private
 
 		def user_params
-			params.require(:user).permit(:name, :email, :answers => [])
+			params.require(:user).permit(:name, :email, {:answers => []})
 		end
 end
