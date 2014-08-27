8e3086a513e6ba55d277309884a4fdc13e6f05ba
mroll <mpr56@drexel.edu>
2014-08-27 14:24:31 -0400

    updated dump_csv function. added capability to adapt to new questions

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,29 +4,7 @@ class AdminController < ApplicationController
   end
 
   def dump_csv
-    @questions = [
-      { "body" => "How many platforms (iOS, Android, Mobile Web)?",
-        "choices"=> { "1"=> 100000, "2"=> 175000, "3"=> 225000 } },
-      { "body"=> "Will your app need photo library access?",
-        "choices"=> { "Yes"=> 5000, "No"=> 0 } },
-      { "body"=> "Will your app need GPS access?",
-        "choices"=> { "Yes"=> 5000, "No"=> 0 } },
-      { "body"=> "Will your app integrate a legacy system?",
-        "choices"=> { "Yes"=> 25000, "No"=> 0 } },
-      { "body"=> "What level of security will your app have?",
-        "choices"=> { "None"=> 0, "Bank-like"=> 25000 } },
-      { "body"=> "Will your app support credit card payment?",
-        "choices"=> { "Yes"=> 0, "No"=> 0 } },
-      { "body"=> "Will your app integrate SalesForce CRM?",
-        "choices"=> { "Yes"=> 25000, "No"=> 0 } },
-      { "body"=> "Will your app function offline?",
-        "choices"=> { "Yes"=> 20000, "No"=> 0 } },
-      { "body"=> "How many ways will users be able to sign in (Twitter, Facebook, LinkedIn, Google)?",
-        "choices"=> { "1"=> 5000, "2"=> 10000, "3"=> 15000, "4"=> 20000, "No sign in"=> 0 } },
-      { "body"=> "Will your app support an MVP or polished design?",
-        "choices"=> { "Polished"=> 50000, "MVP"=> 25000 } }
-    ] 
-    @questions = JSON.parse(@questions.to_json)
+    @questions = Question.all.order(:index)
     @users = User.all
 
       Tempfile.open(["report",".csv"]) do |t|
@@ -41,7 +19,7 @@ class AdminController < ApplicationController
         t << "0".csv_safe
         t << "0".csv_safe
         @questions.each do |q|
-          t << "#{q["body"]}".csv_safe
+          t << "#{q[:body]}".csv_safe
         end
         t << "\n"
         @users.each do |u|
