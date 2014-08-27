637029856a7d4e23300cfde7c57b75e36ede7cab
mroll <mpr56@drexel.edu>
2014-08-26 23:25:57 -0400

    fixed mailer

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -3,7 +3,7 @@ require 'json'
 class NotifyMailer < ActionMailer::Base
   def notify(user)
     @user = user
-    mail :to => "mroll@arcwebtech.com", :from => "mroll@arcwebtech.com", :subject => "EstimatorApp Used"
+    mail :to => "info@arcwebtech.com", :from => "dashboard@arcwebtech.com", :subject => "EstimatorApp Used"
   end
 
   def report(user)
